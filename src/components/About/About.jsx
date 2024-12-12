6c8edf9a26bd8f92ea3fd97abba0b1909b0b9830
Iosu Gómez <131406997+IosuVitoria@users.noreply.github.com>
2024-12-12 18:27:46 +0100

    About traducido

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
 
@@ -6,63 +5,58 @@ import { styles } from '../../styles';
 import { services } from '../../constants';
 import { fadeIn, textVariant } from '../../utils/motion';
 import { SectionWrapper } from '../../hoc';
+import { useTranslation } from 'react-i18next';  // Importar el hook de i18next
 import './About.css';
-
-
 
 const ServiceCard = ({index, title, icon}) => {
   return (
-    <Tilt className = "xs:w-[250px] w-full"> 
+    <Tilt className="xs:w-[250px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-        className='w-full blues-gradient p-[1px] rounded-[20px] shadow-card cursor-pointer'
+        className="w-full blues-gradient p-[1px] rounded-[20px] shadow-card cursor-pointer"
       >
         <div
-          options = {{
+          options={{
             max: 45,
             scale: 1,
             speed: 450
           }}
-
-          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
-          <img src={icon} alt={title} className='w-16 h16 object-contain'/>
-          <h3 className='text-white text-[20px] font-bold text-center '>{title}</h3>
+          <img src={icon} alt={title} className="w-16 h16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center ">{title}</h3>
         </div>
-
       </motion.div>
     </Tilt>
-  )
-}
-
+  );
+};
 
 const About = () => {
+  const { t } = useTranslation(); // Usar el hook para obtener las traducciones
+
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText} style={{marginBottom:"35px", borderBottom: "1px solid white"}} >Overview</h2>
+        <p className={styles.sectionSubText}>{t('introduction')}</p> {/* Traducción del texto */}
+        <h2 className={styles.sectionHeadText} style={{ marginBottom: "35px", borderBottom: "1px solid white" }}>
+          {t('overview')} {/* Traducción del texto */}
+        </h2>
       </motion.div>
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-6xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-6xl leading-[30px]"
       >
-        I am a dedicated full-stack web developer with a background in biology and a strong connection to programming. 
-        My journey began with a master&lsquo;s thesis in bioinformatics and later evolved through diverse experiences, 
-        including working in a laboratory and becoming an Apple-certified technician. These roles honed my analytical skills, adaptability, 
-        and understanding of the business world. Driven by a passion for technology and a desire to pursue my goals, I completed a rigorous bootcamp 
-        and transitioned into full-stack development. Every project I tackle is an opportunity to learn, grow, and bring innovative solutions to life.
+        {t('aboutText')} {/* Traducción del texto largo */}
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index) =>(
-          <ServiceCard key={service.title} index = {index} {...service}/>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
-    
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper(About, "about")
+export default SectionWrapper(About, "about");
--- src/components/i18next/i18n.js
+++ src/components/i18next/i18n.js
@@ -45,7 +45,11 @@ i18n
           hero: {
             title: "Hi, I am",
             subtitle: "My primary role is web development, but I also have experience in Salesforce development."
-          }
+          },
+          // Traducción para About
+          introduction: "Introduction",
+          overview: "Overview",
+          aboutText: "I am a dedicated full-stack web developer with a background in biology and a strong connection to programming. My journey began with a master’s thesis in bioinformatics and later evolved through diverse experiences, including working in a laboratory and becoming an Apple-certified technician. These roles honed my analytical skills, adaptability, and understanding of the business world. Driven by a passion for technology and a desire to pursue my goals, I completed a rigorous bootcamp and transitioned into full-stack development. Every project I tackle is an opportunity to learn, grow, and bring innovative solutions to life.",
         },
       },
       es: {
@@ -76,15 +80,16 @@ i18n
             git: "Git",
             salesforce: "Salesforce"
           },
-          // Traducción para el Hero
           hero: {
             title: "Hola, soy",
             subtitle: "Mi rol principal es el desarrollo web, pero también tengo experiencia en desarrollo de Salesforce."
-          }
+          },
+          introduction: "Introducción",
+          overview: "Motivaciones personales",
+          aboutText: "Soy un desarrollador web full-stack con formación de biólogo y con una fuerte conexión con la programación desde el principio. Mi camino comenzó con una tesis de máster en bioinformática. Luego evolucioné a través de diversas experiencias laborales que incluyeron trabajar en un laboratorio y convertirme en técnico certificado de Apple. Estos roles perfeccionaron mis habilidades analíticas, adaptabilidad y comprensión del mundo empresarial. Impulsado por una pasión por la tecnología y el deseo de alcanzar mis metas, completé un bootcamp riguroso y hice la transición al desarrollo full-stack. Cada proyecto que emprendo es una oportunidad para aprender, crecer y hacer realidad soluciones innovadoras.",
         },
       },
     },
   });
 
 export default i18n;
-
